8b64b4aa2319989d6369bbb5fc3a27b740ed183b
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-22 17:36:04 -0400

    [CHG] Se agrega ruta de tareas y resuelve error en los cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { dbConnection } = require('./database/config');
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 dbConnection();
 
@@ -16,9 +16,8 @@ dbConnection();
  */
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
-//app.use('/api/tasks', require('./routes/tasks'));
+app.use('/api/tasks', require('./routes/tasks'));
 
 app.listen(process.env.PORT, () => {
   console.log('Servidor corriendo en el puerto ' + process.env.PORT);
 });
-
