8eec3a746ebe04f259179c0d36ee6f32c0f64ca5
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-22 17:38:09 -0400

    [CHG] Se implementan validaciones

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 
-const login = require('../controllers/auth');
+const { login } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validateFields');
 
 const router = Router();
 
-router.post('/', login);
+router.post('/', [
+  check('name', 'El nombre de usuario es requerido').not().isEmpty(),
+  check('password', 'La contraseña es requerida').not().isEmpty(),
+  validateFields
+], login);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { getUsers, registerUser } = require('../controllers/users');
+const { validateFields } = require('../middlewares/validateFields');
 
 const router = Router();
 
 router.get('/', getUsers);
-router.post('/', registerUser);
+
+router.post('/', [
+  check('name', 'El nombre es requerido').not().isEmpty(),
+  check('username', 'El nombre de usuario es requerido').not().isEmpty(),
+  check('password', 'La contraseña es requerida').not().isEmpty(),
+  check('password', 'La contraseña es invalida').isLength({ min: 5, max: 10}),
+  validateFields
+], registerUser);
 
 module.exports = router;
